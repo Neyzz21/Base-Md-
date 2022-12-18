e2dafdeb05c0681ced5749c902e7c0ea3186c960
Neyzz21 <107815428+Neyzz21@users.noreply.github.com>
2022-12-18 08:33:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -35,11 +35,6 @@ conn.ev.on('creds.update', () => saveState)
 
 conn.reply = (from, content, msg) => conn.sendMessage(from, { text: content }, { quoted: msg })
 
-conn.ws.on('CB:call', async (json) => {
-const user_Call = json.content[0].attrs['call-creator']
-conn.sendMessage(user_Call, { text: 'Maaf kamu terdeteksi telepon bot!\n5 detik lagi kamu akan,\ndiblokir otomatis oleh bot.'})
-await sleep(5000)
-conn.updateBlockStatus(user_Call, 'block')
 })
 
 conn.ev.on('connection.update', (update) => {
